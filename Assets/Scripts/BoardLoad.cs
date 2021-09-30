4a22640f31b6488323a40ab848719a8054fa9cbb
dim1000127 <60386475+dim1000127@users.noreply.github.com>
2021-09-30 21:55:38 +0300

    Отображение поля 9x9 с кристаллами пяти видов
    Сделано корректное отображение поля с кристаллами пяти видов. При генерации поля исключено появление одного кристалла три в ряд.

--- Assets/Scripts/BoardLoad.cs
+++ Assets/Scripts/BoardLoad.cs
@@ -4,40 +4,58 @@ using UnityEngine;
 
 public class BoardLoad : MonoBehaviour
 {
-    public int[,] board;
-    public GameObject[] blocks;
-    //public GameObject tile;
+    public GameObject[,] board;
+    public List<GameObject> blocks = new List<GameObject>();
+
     //размерность доски
-    public int xSize; 
+    public int xSize;
     public int ySize;
 
     void Start()
     {
-
-        board = new int[xSize, ySize];
-        //Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
-        CreateBoard();
+        board = new GameObject[xSize, ySize];
+        //вычисление размера игрового объекта(гема)
+        Vector2 offset = blocks[4].GetComponent<MeshRenderer>().bounds.size;
+        //print(offset.x);
+        //print(offset.y);
+        //для расстояния между гемами прибавляется 0.15
+        CreateBoard(offset.x + (float)0.15, offset.y + (float)0.15);
     }
 
     void Update()
     {
-        
+
     }
 
-    void CreateBoard()
+    void CreateBoard(float xSizeGem, float ySizeGem)
     {
+        float startX = transform.position.x;     
+        float startY = transform.position.y;
+
+        //массив для хранения добавленных гемов слева
+        GameObject[] gemsLeft = new GameObject[ySize];
+        //массив для хранения добавленного гема внизу
+        GameObject gemsBottom = null;
+        GameObject tempBlock;
         for (int x = 0; x < board.GetLength(0); x++)
         {
             for (int y = 0; y < board.GetLength(1); y++)
             {
-                int randNum = Random.Range(0, blocks.Length); //получаем номер рандомного гема из массива гемов
-                GameObject gem = (GameObject)Instantiate(blocks[randNum], new Vector3(x, y, 10), blocks[randNum].transform.rotation) as GameObject;
+                //новый List(копия листа с гемами) для динмической работы с ним
+                List<GameObject> checkBlocks = new List<GameObject>();
+                checkBlocks.AddRange(blocks);
+                //удаление из List элементов находящ. слева и внизу
+                checkBlocks.Remove(gemsLeft[y]);
+                checkBlocks.Remove(gemsBottom);
+                //print(checkBlocks.Count);
+                int randNum = Random.Range(0, checkBlocks.Count); //номер рандомного гема из листа гемов
+                tempBlock = checkBlocks[randNum];
+                //добавление гема в игровое пространство
+                Instantiate(tempBlock, new Vector3(startX + (xSizeGem * x), startY + (ySizeGem * y), 20), tempBlock.transform.rotation, transform);
 
-                board[x, y] = randNum;
-                Gems b = gem.gameObject.AddComponent<Gems>();
-                b.x = x;
-                b.y = y;
-                b.ID = randNum;
+                board[x, y] = tempBlock;
+                gemsLeft[y] = tempBlock;
+                gemsBottom = tempBlock;
             }
         }
     }
